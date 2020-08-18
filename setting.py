a2b65d4151425060ec4925a8a7d4f112b060eee0
umersaeedbajwa <66518793+umersaeedbajwa@users.noreply.github.com>
2020-08-18 10:49:20 +0500

    Initial commit

---
+++ setting.py
@@ -0,0 +1,6 @@
+class Setting():
+    def __init__(self):
+        self.screen_width = 800
+        self.screen_height = 400
+        self.bg_color = (22, 252, 122)
+        self.ship_speed_factor = 1.5
---
+++ ship.py
@@ -0,0 +1,33 @@
+import pygame
+import setting
+class Ship():
+
+    def __init__(self, ai_settings, screen):
+
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        self.image = pygame.image.load('Images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+        self.center = float(self.rect.centerx)
+
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+
+        if self.moving_right:
+            self.center += self.ai_settings.ship_speed_factor
+
+        if self.moving_left:
+            self.center -= self.ai_settings.ship_speed_factor
+
+        self.rect.centerx = self.center
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
